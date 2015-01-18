a9723b6b786bf0aa906d3e3e46e57e5d1bc77fd2
GeStanley <geoffrey.stanley@gmail.com>
2015-01-17 23:04:11 -0500

    added column of ones

--- load_data.py
+++ load_data.py
@@ -3,13 +3,18 @@ __author__ = 'geoffrey'
 import numpy
 import pylab
 
-my_array_x = numpy.loadtxt('hw2x.dat', float)
-my_array_y = numpy.loadtxt('hw2y.dat', float)
+array_x = numpy.loadtxt('hw1x.dat', float)
+vector_y = numpy.loadtxt('hw1y.dat', float)
+
+array_ones = numpy.ones((array_x[:, 0].size, 1))
+
+array_x = numpy.append(array_x, array_ones, axis=1)
+
+matrix_x = numpy.matrix(array_x)
+
+print matrix_x
+print type(matrix_x)
 
 
-print my_array_x[:, 0]
-print my_array_y.size
-
-
-pylab.scatter(my_array_x[:, 1], my_array_y)
+pylab.scatter(array_x[:, 1], vector_y)
 pylab.show()
