da1864ed3ce267d7d011f6db7978c34758e41861
GeStanley <geoffrey.stanley@gmail.com>
2015-01-18 00:42:56 -0500

    regression working

--- load_data.py
+++ load_data.py
@@ -1,7 +1,10 @@
 __author__ = 'geoffrey'
 
 import numpy
-import pylab
+import matplotlib.pyplot as plt
+from sklearn.preprocessing import PolynomialFeatures
+from sklearn.linear_model import LinearRegression
+from sklearn.pipeline import Pipeline
 
 array_x = numpy.loadtxt('hw1x.dat', float)
 vector_y = numpy.loadtxt('hw1y.dat', float)
@@ -10,11 +13,36 @@ array_ones = numpy.ones((array_x[:, 0].size, 1))
 
 array_x = numpy.append(array_x, array_ones, axis=1)
 
-matrix_x = numpy.matrix(array_x)
+#matrix_x = numpy.matrix(array_x)
 
-print matrix_x
-print type(matrix_x)
+# print array_x[:, 0].shape
+# print type(array_x[:, 0])
+# print vector_y.shape
+# print type(vector_y)
+
+poly = PolynomialFeatures(degree=2)
+
+model = Pipeline([('poly', poly),
+                ('linear', LinearRegression())])
 
 
-pylab.scatter(array_x[:, 1], vector_y)
-pylab.show()
+
+
+model.fit(array_x[:, 1, numpy.newaxis], vector_y[:, numpy.newaxis])
+
+
+# Plot outputs
+plt.scatter(array_x[:, 1], vector_y[:, numpy.newaxis],  color='black')
+
+print array_x[:, 1, numpy.newaxis]
+print model.predict(array_x[:, 1, numpy.newaxis])
+
+sorted = numpy.sort(array_x[:, 1, numpy.newaxis], axis=0)
+print sorted
+
+plt.plot(sorted, model.predict(sorted), color='blue', linewidth=3)
+
+# plt.xticks(())
+# plt.yticks(())
+
+plt.show()
